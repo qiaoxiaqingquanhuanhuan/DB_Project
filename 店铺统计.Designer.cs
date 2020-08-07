a1fa8a7f87fc92e2174ce9e056af18f434a1775c
RexsA <38357959+zhebafeichangwen@users.noreply.github.com>
2020-08-07 10:54:14 +0800

    Add files via upload

---
+++ 店铺统计.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace 管理员
+{
+    partial class 店铺统计
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_各种类店铺销量 = new System.Windows.Forms.Button();
+            this.button_各种类店铺销售额 = new System.Windows.Forms.Button();
+            this.button_店铺销量 = new System.Windows.Forms.Button();
+            this.button_店铺销售额 = new System.Windows.Forms.Button();
+            this.button_店铺评价 = new System.Windows.Forms.Button();
+            this.button_return = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_各种类店铺销量
+            // 
+            this.button_各种类店铺销量.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_各种类店铺销量.Font = new System.Drawing.Font("楷体", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_各种类店铺销量.Location = new System.Drawing.Point(200, 100);
+            this.button_各种类店铺销量.Name = "button_各种类店铺销量";
+            this.button_各种类店铺销量.Size = new System.Drawing.Size(300, 75);
+            this.button_各种类店铺销量.TabIndex = 2;
+            this.button_各种类店铺销量.Text = "各种类店铺销量";
+            this.button_各种类店铺销量.UseVisualStyleBackColor = false;
+            this.button_各种类店铺销量.Click += new System.EventHandler(this.button_各种类店铺销量_Click);
+            // 
+            // button_各种类店铺销售额
+            // 
+            this.button_各种类店铺销售额.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_各种类店铺销售额.Font = new System.Drawing.Font("楷体", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_各种类店铺销售额.Location = new System.Drawing.Point(700, 100);
+            this.button_各种类店铺销售额.Name = "button_各种类店铺销售额";
+            this.button_各种类店铺销售额.Size = new System.Drawing.Size(300, 75);
+            this.button_各种类店铺销售额.TabIndex = 3;
+            this.button_各种类店铺销售额.Text = "各种类店铺销售额";
+            this.button_各种类店铺销售额.UseVisualStyleBackColor = false;
+            this.button_各种类店铺销售额.Click += new System.EventHandler(this.button_各种类店铺销售额_Click);
+            // 
+            // button_店铺销量
+            // 
+            this.button_店铺销量.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_店铺销量.Font = new System.Drawing.Font("楷体", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_店铺销量.Location = new System.Drawing.Point(200, 250);
+            this.button_店铺销量.Name = "button_店铺销量";
+            this.button_店铺销量.Size = new System.Drawing.Size(300, 75);
+            this.button_店铺销量.TabIndex = 4;
+            this.button_店铺销量.Text = "店铺销量";
+            this.button_店铺销量.UseVisualStyleBackColor = false;
+            this.button_店铺销量.Click += new System.EventHandler(this.button_店铺销量_Click);
+            // 
+            // button_店铺销售额
+            // 
+            this.button_店铺销售额.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_店铺销售额.Font = new System.Drawing.Font("楷体", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_店铺销售额.Location = new System.Drawing.Point(700, 250);
+            this.button_店铺销售额.Name = "button_店铺销售额";
+            this.button_店铺销售额.Size = new System.Drawing.Size(300, 75);
+            this.button_店铺销售额.TabIndex = 5;
+            this.button_店铺销售额.Text = "店铺销售额";
+            this.button_店铺销售额.UseVisualStyleBackColor = false;
+            this.button_店铺销售额.Click += new System.EventHandler(this.button_店铺销售额_Click);
+            // 
+            // button_店铺评价
+            // 
+            this.button_店铺评价.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_店铺评价.Font = new System.Drawing.Font("楷体", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_店铺评价.Location = new System.Drawing.Point(200, 400);
+            this.button_店铺评价.Name = "button_店铺评价";
+            this.button_店铺评价.Size = new System.Drawing.Size(300, 75);
+            this.button_店铺评价.TabIndex = 6;
+            this.button_店铺评价.Text = "店铺评价";
+            this.button_店铺评价.UseVisualStyleBackColor = false;
+            this.button_店铺评价.Click += new System.EventHandler(this.button_店铺评价_Click);
+            // 
+            // button_return
+            // 
+            this.button_return.AutoSize = true;
+            this.button_return.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button_return.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_return.Location = new System.Drawing.Point(100, 550);
+            this.button_return.Name = "button_return";
+            this.button_return.Size = new System.Drawing.Size(150, 50);
+            this.button_return.TabIndex = 7;
+            this.button_return.Text = "返回";
+            this.button_return.UseVisualStyleBackColor = false;
+            this.button_return.Click += new System.EventHandler(this.button_return_Click);
+            // 
+            // 店铺统计
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 644);
+            this.Controls.Add(this.button_return);
+            this.Controls.Add(this.button_店铺评价);
+            this.Controls.Add(this.button_店铺销售额);
+            this.Controls.Add(this.button_店铺销量);
+            this.Controls.Add(this.button_各种类店铺销售额);
+            this.Controls.Add(this.button_各种类店铺销量);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "店铺统计";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "店铺统计";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.店铺统计_FormClosed);
+            this.Load += new System.EventHandler(this.店铺统计_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_各种类店铺销量;
+        private System.Windows.Forms.Button button_各种类店铺销售额;
+        private System.Windows.Forms.Button button_店铺销量;
+        private System.Windows.Forms.Button button_店铺销售额;
+        private System.Windows.Forms.Button button_店铺评价;
+        private System.Windows.Forms.Button button_return;
+    }
+}
---
+++ 我的信息.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 管理员
+{
+    public partial class 我的信息 : Form
+    {
+        public 我的信息()
+        {
+            InitializeComponent();
+        }
+        //加载“我的信息”窗体
+        private void 我的信息_Load(object sender, EventArgs e)
+        {  
+            this.Owner.Hide();//隐藏所有者窗体，即“管理员”窗体
+            
+            //查询我的信息
+            DB我的信息 db = new DB我的信息();
+            label_ID_data.Text = db.id.ToString();//显示ID
+            label_adm_name_data.Text = db.name.ToString();//显示姓名
+        }
+        //关闭“我的信息”窗体后
+        private void 我的信息_FormClosed(object sender, FormClosedEventArgs e)
+        {  
+            this.Owner.Show();//显示所有者窗体，即“管理员”窗体
+        }
+        //返回所有者窗体，即“管理员”窗体
+        private void button_return_Click(object sender, EventArgs e)
+        {
+            this.Close();//关闭当前窗体
+        }
+    }
+}
