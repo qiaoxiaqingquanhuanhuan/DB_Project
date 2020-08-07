fb35e87fee64e365096e5251c831147cf2dfeed2
RexsA <38357959+zhebafeichangwen@users.noreply.github.com>
2020-08-07 10:57:40 +0800

    Add files via upload

---
+++ DB我的信息.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace 管理员
+{
+    class DB我的信息
+    {
+        private const String connStr = "server=localhost;database=class_1;uid=root;pwd=12345678";//连接数据库的基本信息
+
+        public int id;//管理员ID
+        public char name;//管理员姓名
+        public void get_name(int id)//通过ID获取姓名
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand())//创建查询命令
+                {
+                    string sql = "select * from administrator where ID=@id";//sql语句
+                    cmd.Connection = conn;
+                    cmd.CommandText = sql;
+                    MySqlDataReader rd = cmd.ExecuteReader();//执行查询操作
+                    while (rd.Read())//每次循环查询一行
+                    {
+                        name = rd.GetChar("adm_name");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 查询结果.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace 管理员
+{
+    partial class 查询结果
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_return = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_return
+            // 
+            this.button_return.AutoSize = true;
+            this.button_return.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button_return.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_return.Location = new System.Drawing.Point(50, 50);
+            this.button_return.Name = "button_return";
+            this.button_return.Size = new System.Drawing.Size(150, 50);
+            this.button_return.TabIndex = 9;
+            this.button_return.Text = "返回";
+            this.button_return.UseVisualStyleBackColor = false;
+            this.button_return.Click += new System.EventHandler(this.button_return_Click);
+            // 
+            // 查询结果
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_return);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "查询结果";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "查询结果";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.查询结果_FormClosed);
+            this.Load += new System.EventHandler(this.查询结果_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_return;
+    }
+}
